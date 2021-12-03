3441afae0c14e576806db8414b1a79ca22b85327
JuliaKeiter <Julia.Keiter@gmx.de>
2021-12-03 12:42:13 -0700

    randomforest test

---
+++ R/randomforest.R
@@ -0,0 +1,19 @@
+library(sumcat)
+library(randomForest)
+library(party)
+
+str(water_potability)
+
+set.seed(1229)
+
+model <- randomForest(
+  formula = as.factor(Potability) ~ .,
+  data = water_potability
+)
+
+print(model)
+print(importance(model,type = 2))
+
+#predict Potability from water_test
+what <- predict(model, newdata=water_test[,-10])
+
