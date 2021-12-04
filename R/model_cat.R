799aee8d90e3efe037e8f2185e71ec5f68a268c0
Sam Haycock <sam.haycock@usu.edu>
2021-12-03 17:13:35 -0700

    more mod

--- R/model_cat.R
+++ R/model_cat.R
@@ -7,6 +7,11 @@
 model_cat <- function(formula, data){
   log_model <- glm(formula = formula, data = data, family = "binomial")
 
-  rf_model <- randomForest::randomForest(factor(Potability) ~.,
-                                         data = water_potability)
+  rf_model <- randomForest::randomForest(formula, data = data)
+
+  svm_model_frame <- model.frame(formula, data = data)
+  y <- svm_model_frame[, 1]
+  x <- svm_model_frame[, 2:ncol(svm_model_frame)]
+  svm_model <- EZtune::eztune(x, y, method = "svm")
+
 }
