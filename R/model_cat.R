7de2af52a92aedb662a6778db0bf4fe59e898d08
Sam Haycock <sam.haycock@usu.edu>
2021-12-03 17:22:47 -0700

    model_cat is working

--- R/model_cat.R
+++ R/model_cat.R
@@ -4,6 +4,9 @@
 #'   for each type: Random Forest, Logistic Regression, LDA, SVM.
 #' @param formula A formula statement
 #' @param data A categorical data set specified by the user
+#'
+#' @return A list object that contains the ensemble of models
+#' @export
 model_cat <- function(formula, data){
   log_model <- glm(formula = formula, data = data, family = "binomial")
 
@@ -14,4 +17,11 @@ model_cat <- function(formula, data){
   x <- svm_model_frame[, 2:ncol(svm_model_frame)]
   svm_model <- EZtune::eztune(x, y, method = "svm")
 
+  lda_model <- MASS::lda(formula, data = data)
+
+  object <- list(log_model, rf_model, svm_model, lda_model)
+
+  class(object) <- "sumcat"
+
+  object
 }
