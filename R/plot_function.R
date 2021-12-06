603202808bdc9b048456b36a4621cc1b001c8411
JuliaKeiter <Julia.Keiter@gmx.de>
2021-12-06 16:40:20 -0700

    plot

--- R/plot_function.R
+++ R/plot_function.R
@@ -10,29 +10,21 @@
 #'@param test A dataset, which is used to test the models in order to
 #'create predictions
 #'
+#'@export
+plot <- function(x){
 
-plot.sumcat <- function(x,train,test){
-  class_methods <- vector("list", 3)
+  residual <- vector("list", length(x))
 
-  class_methods[[1]] <- MASS::lda(x, data = train)
-  class_methods[[2]] <- randomForest::randomForest(x, data = train)
-  class_methods[[3]] <- glm(x, data = train)
-
-  predictions <- vector("list", length(class_methods))
-  residual <- vector("list", length(class_methods))
-
-  for (i in 1:length(class_methods)) {
-
-    predictions[[i]] <- predict(class_methods[[i]],
-                                           newdata = test[,-10])
-    residual[[i]] <- as.numeric(test[,10]) - as.numeric(predictions[[i]][[class]])
+  for(i in 1:length(x)){
+    residual[i] <- x[[i]]$Fitted - x[[i]]$Prediction
   }
 
   h <- do.call(cbind, residual)
-  h.melt <- reshape2::melt(h)
+  h.melt <- reshape::melt(h)
 
   #densityplot
-  gglot2::ggplot(h.melt,aes(x=class_methods[[1]],fill=residual, col = residual))+
+  gglot2::ggplot(h.melt,aes(x=class_methods[[1]],
+                            fill=residual, col = residual))+
     geom_density()+
     facet_grid(residual~.)+
     geom_rug()
